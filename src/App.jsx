6edd84973135b5f8ee947524cbb9832c7bfb24e1
unknown <camilobeltran174@gmail.com>
2023-05-08 21:24:01 -0500

    commit to publish in gp

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Header from './Header'
 import Info from './Info'
 import Body from './Body'
 import Footer from './Footer'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
     return (
