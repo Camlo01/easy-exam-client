c2dec8d340e4ccc34bfe9061bdd9cc60311e2894
unknown <camilobeltran174@gmail.com>
2023-05-28 18:19:50 -0500

    Indicate new version

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,16 +1,18 @@
 export default function Footer() {
-    console.log("Versión de aplicación 1.0 de 23/Mayo/2023")
+    console.log("Versión de aplicación 1.2  28/Mayo/2023")
     console.log("Autor: Camilo A. Beltrán")
     return (
         <>
-            <footer> <small>
-                Herramienta de creación de examenes
-                <br />
-                versión 1.1
-                {/* &copy; Camilo Beltrán  */}
-                {/* Autor: Camilo Beltrán */}
-                <br />
-            </small> </footer>
+            <footer>
+                <small>
+                    Herramienta de creación de examenes
+                    <br />
+                    versión 1.2
+                    {/* &copy; Camilo Beltrán  */}
+                    {/* Autor: Camilo Beltrán */}
+                    <br />
+                </small>
+            </footer>
         </>
     )
 
