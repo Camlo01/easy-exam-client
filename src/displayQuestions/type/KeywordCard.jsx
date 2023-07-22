3df26532d5dc5b95b5c06f9a1a7da1d9d904c70e
Camilo <camilobeltran174@gmail.com>
2023-07-21 20:37:51 -0500

    add question keywords

--- src/displayQuestions/type/KeywordCard.jsx
+++ src/displayQuestions/type/KeywordCard.jsx
@@ -6,6 +6,7 @@ export default function KeywordCard({ question, deleteQuestion, i }) {
         <div className="question-card green-card">
             <h2>#{i} Pregunta de palabras claves</h2>
             <p>{question.questionText}</p>
+            <p>{question.answer1}, {question.answer2}, {question.answer3}, {question.answer4} </p>
             <div className="card-button--container">
                 <button onClick={() => deleteQuestion(question.id ?? 99)}>Eliminar</button>
             </div>
