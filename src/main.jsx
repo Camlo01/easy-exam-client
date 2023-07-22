133c17bda8e73f1bed049925300501503d52d02a
Camilo <camilobeltran174@gmail.com>
2023-07-21 20:27:13 -0500

    add router with react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import VisualizeData from './pages/visualizeData'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/easy-exam-client",
+    element: <App />
+  }
+  ,
+  {
+    path: "/easy-exam-client/data",
+    element: <VisualizeData />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
