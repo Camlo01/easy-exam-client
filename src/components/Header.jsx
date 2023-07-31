019c0269565a5d2504b93f3038c013bc89616f15
Camilo <camilobeltran174@gmail.com>
2023-07-31 16:31:49 -0500

    Implement 'react-router-dom' and Link buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
+import { Link } from 'react-router-dom'
 import './Header.css'
 
 export default function Header() {
     return (
         <div className="header-header">
-            <h1>Website easy-exam</h1>
+
+            <div>
+                <h1>Website easy-exam</h1>
+            </div>
+
+            <div>
+                <Link to={"/easy-exam-client/"}>
+                    <button className="yellow-button">Generar exámenes</button>
+                </Link>
+                <Link to={"/easy-exam-client/data"}>
+                    <button className="yellow-button">Generar gráficos</button>
+                </Link>
+            </div>
+
         </div>
     )
 }
