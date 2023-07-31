0f721252cbb085018bddd160ac0b2deb9e7b110d
Camilo <camilobeltran174@gmail.com>
2023-07-31 16:24:03 -0500

    Add class with utils methods | Implement method  for validate theres no repeat values

---
+++ src/resources/Util.jsx
@@ -0,0 +1,28 @@
+
+/**
+ * Method for validate theres no value repeat in any field
+ * @param {*} arr of text fields
+ * @returns boolean value
+ */
+export default function validateNonRepeatFields(arr) {
+
+    let isAnyValueRepeat = false;
+
+    for (let i = 0; i < arr.length && !isAnyValueRepeat; i++) {
+
+        let counterRepeatOption = 0;
+
+        for (let j = 0; j < arr.length && isAnyValueRepeat != true; j++) {
+
+            if (Object.is(arr[i], arr[j])) {
+                counterRepeatOption++;
+            }
+
+            if (counterRepeatOption > 1) {
+                isAnyValueRepeat = true;
+            }
+        }
+    }
+
+    return !(isAnyValueRepeat);
+}
