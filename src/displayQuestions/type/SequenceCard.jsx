1c18e7828da856f1d7fdbe12c993c71f92354d4c
Camilo <camilobeltran174@gmail.com>
2023-08-25 14:05:43 -0500

    Spell Check

--- src/displayQuestions/type/SequenceCard.jsx
+++ src/displayQuestions/type/SequenceCard.jsx
@@ -4,7 +4,7 @@ export default function SequenceCard({ question, deleteQuestion, i }) {
 
     return (
         <div className="question-card yellow-card">
-            <h2>#{i} Pregunta de ordenar</h2>
+            <h2>#{i} Pregunta de Ordenar</h2>
             <p>{question.questionText}</p>
             <p>1. {question.sequenceText1}</p>
             <p>2. {question.sequenceText2}</p>
