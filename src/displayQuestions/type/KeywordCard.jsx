345f39b2292ccef7809d96cec0a0c6060cdf4d68
Camilo <camilobeltran174@gmail.com>
2023-08-25 14:04:51 -0500

    improve styles

--- src/displayQuestions/type/KeywordCard.jsx
+++ src/displayQuestions/type/KeywordCard.jsx
@@ -4,9 +4,9 @@ export default function KeywordCard({ question, deleteQuestion, i }) {
 
     return (
         <div className="question-card green-card">
-            <h2>#{i} Pregunta de palabras claves</h2>
+            <h2>#{i} Pregunta Palabras Claves</h2>
             <p>{question.questionText}</p>
-            <p>{question.answer1}, {question.answer2}, {question.answer3}, {question.answer4} </p>
+            <p>Palabrass claves: {question.answer1}, {question.answer2}, {question.answer3}, {question.answer4} </p>
             <div className="card-button--container">
                 <button onClick={() => deleteQuestion(question.id ?? 99)}>Eliminar</button>
             </div>
