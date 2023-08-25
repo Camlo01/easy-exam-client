345dc9f834d9f8e0ada6e2ff2ebcc3a5d089f5a7
Camilo <camilobeltran174@gmail.com>
2023-08-25 10:16:36 -0500

    Improved content

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,8 +6,11 @@ export default function Info() {
         <>
             <div className="body-text">
                 <div className="body-text-card">
-                    <h1>Bienvenido!</h1>
-                    <p>Sitio web creado para generar exámenes y exportarlos</p>
+                    <h1>¡Bienvenido!</h1>
+                    <p>Sitio Web creado para generar evaluaciones para cursos en Campus GMA</p>
+                </div>
+                <div className="body-text-content">
+                    <p>A continuación encontrarás los tipos de preguntas existentes en la plataforma, despliega la lista y empieza la creación de preguntas:</p>
                 </div>
             </div>
         </>
